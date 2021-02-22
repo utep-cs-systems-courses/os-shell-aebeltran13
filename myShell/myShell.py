98a0287bc05638b0c6f2bfaf272ed7b370750464
aebeltran13 <aebeltran13@gmail.com>
2021-02-21 22:12:06 -0700

    Updating shell. Added redirection and piping

--- myShell/myShell.py
+++ myShell/myShell.py
@@ -22,28 +22,137 @@ def execute(args):
         except: #file directory does not exist
             os.write(1, ("cd %s: Directory does not exists") %arg[1].encode())
             pass
+    #piping
+    elif "|" in args:
+        pipe(args)
     else:
         rc = os.fork()
-
-        if rc < 0:
+        background = True
+        #if & in args, then its in background
+        if "&" in args:
+            args.remove("&")
+            background = False
+        elif rc < 0:
             os.write(2, ("fork failed, returning %d\n" % rx).encode())
             sys.exit(1)
         elif rc == 0:
-            #trying each directory in path
-            for dir in re.split(":", os.environ['PATH']):
-                program = "%s/%s" % (dir, args[0])
+            #execute specified program
+            if "/" in args[0]:
+                program = args[0]
                 try:
-                    os.execve(program, args, os.environ) #trying to execute
+                    os.execve(program, args, os.environ)
                 except FileNotFoundError:
                     pass
+            #Redirect happens
+            elif ">" in args or "<" in args:
+                redirect(args)
+            #No specifications, just execute program
+            else:
+                #trying each directory in path
+                for dir in re.split(":", os.environ['PATH']):
+                    program = "%s/%s" % (dir, args[0])
+                    try:
+                        os.execve(program, args, os.environ) #trying to execute
+                    except FileNotFoundError:
+                        pass
 
             #could not execute
             os.write(2, ("Could not execute %s\n" %args[0]).encode())
             sys.exit(1) #terminate with error
 
         else:
-            os.wait()
- 
+            #if parent running in background we wait for child
+            if background == True:
+                os.wait()
+
+#redirect
+def redirect(args):
+    #fd 1
+    if '>' in args:
+        #redirect child's stdout
+        os.close(1)
+        os.open(args[args.index('>')+1], os.O_CREAT | os.O_WRONLY)
+        os.set_inheritable(1,True)
+        #remove '>' symbol from input
+        args.remove(args[args.index('>') +1]) #space after '>'
+        args.remove('>')
+    #fd 0
+    if '<' in args:
+        #redirect child's stdin
+        os.close(0)
+        os.open(args[args.index('<') +1], os.O_RDONLY)
+        os.set_inheritable(0,True)
+        #remove '<' symbol from input
+        args.remove(args[args.index('<') +1]) #space after '<'
+        args.remove('<')
+
+    #trying each directory in Path
+    for dir in re.split(":", os.environ['PATH']):
+        program = "%s/%s" % (dir,args[0])
+        try:
+            os.execve(program,args,os.environ)
+        except FileNotFoundError:
+            pass
+    os.write(2, ("%s: Command not found \n" % args[0]).encode())
+    sys.exit(0)
+
+#pipe
+def pipe(args):
+    pr, pw = os.pipe()
+    #left side of pipe
+    left = args[0:args.index("|")]
+    right = args[args.index("|") +1:] #everything afte '|'
+
+    #forking
+    rc = os.fork()
+
+    if rc < 0:
+        os.write(2, ("Fork failed, returning %d\n" % rc).encode())
+        sys.exit(1)
+    #child
+    elif rc == 0:
+        os.close(1) #redirect child's stdout
+        os.dup(pw)
+        os.set_inheritable(1, True)
+        for fd in (pr, pw):
+            os.close(fd)
+        command(left)
+        os.write(2, ("Could not execute %s\n" %left).encode())
+        sys.exit(1)
+    else:
+        os.close(0)
+        os.dup(pr)
+        os.set_inheritable(0, True)
+        for fd in (pr, pw):
+            os.close(fd)
+        if "|" in right: #recursion in case of a | b | c | d | ...
+            pipe(right)
+        command(right)
+        os.write(2, ("Could not execute %s\n" %right).encode())
+        sys.exit(1)
+
+#command
+def command(args):
+    if "/" in args[0]:
+        program = args[0]
+        try:
+            os.execve(program, args, os.environ)
+        except FileNotFoundError:
+            pass
+
+    #if redirection happens
+    elif ">" in args or "<" in args:
+        redirect(args)
+    else:
+        for dir in re.split(":", os.environ['PATH']):
+            program = "%s/%s" % (dir, args[0])
+            try:
+                os.execve(program, args, os.environ)
+            except FileNotFoundError:
+                pass
+    os.write(2, ("%s: Command not found\n" % args[0]).encode())
+    sys.exit(0)
+
 #main shell
 while True:
     if 'PS1' in os.environ:
